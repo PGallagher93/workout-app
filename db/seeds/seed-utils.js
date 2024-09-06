a912ce9ffb1667892d5e5ddac188fe9655269406
PGallagher93 <Pgallagher93@hotmail.com>
2024-09-06 12:53:17 +0100

    adds seed utils

---
+++ db/seeds/seed-utils.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+
+exports.addExerciseID = (exerciseData) =>{
+    
+    let count = 1
+    let formattedData = exerciseData.map((exercise) => {
+        exercise.exercise_id = count
+        count ++
+        return exercise
+    })
+    
+    return formattedData
+}
+
+exports.findExerciseID = (workoutStatsData, formattedExerciseData) => {
+    let formattedData = workoutStatsData.map((workout) => {
+        
+        formattedExerciseData.forEach((exercise) => {
+            
+            if(exercise.exercise_name === workout.exercise_name){
+                
+                workout.exercise_id = exercise.exercise_id
+            }
+            
+        })
+        return workout
+    })
+    return formattedData
+}
+exports.hashPassword = (userData) => {
+       const hashedUsers = userData.map((user) => {
+        
+        return user.password = bcrypt.hash(user.password, 10)
+       })
+       return Promise.all(hashedUsers)
+}
