8dac62b929f3d27300acd21f04ede4d2017f3eec
PGallagher93 <Pgallagher93@hotmail.com>
2024-09-13 13:51:49 +0100

    adds exercise records test data

--- db/test-data/exercise-records.js
+++ db/test-data/exercise-records.js
@@ -1,4 +1,8 @@
 module.exports = [
     {exercise_id: "Barbell Bench Press",
-    weight: 60,}
+    weight: 60,
+    user_id: 1},
+    {exercise_id: "Barbell Bench Press",
+     weight: 70,
+    user_id: 1}
 ]
